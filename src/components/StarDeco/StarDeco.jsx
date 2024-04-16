58983ada44a6ef31cf533f81fa818b2878e33af5
Ahmed-Waseem77 <ahmedwaseemr@aucegypt.edu>
2024-04-16 18:15:05 +0200

    background deco

--- src/components/StarDeco/StarDeco.jsx
+++ src/components/StarDeco/StarDeco.jsx
@@ -5,21 +5,23 @@ import { ReactComponent as STAR } from '../../assets/AYON_STAR_D.svg';
 const StarDeco = () => {
 	return ( 
 		<>
-			<div className='starDeco-container' style={{right:'-2vw'}}>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-		</div>
-			<div className='starDeco-container' style={{right:'5vw'}} >
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
-			<STAR className='starDeco-star'/>
+			<div className='starDeco-container'>
+			<div className='starDeco-subcontainer'>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+			</div>
+			<div className='starDeco-subcontainer'>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+				<STAR className='starDeco-star'/>
+			</div>
 		</div>
 		</>
 	);
